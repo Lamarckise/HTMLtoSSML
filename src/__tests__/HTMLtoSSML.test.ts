6e9fab68b7e1b05b067af41e1623de9a63902958
Axel Peigné <axel@lamarckise.fr>
2021-01-17 15:08:41 +0100

    Prettied

--- src/__tests__/HTMLtoSSML.test.ts
+++ src/__tests__/HTMLtoSSML.test.ts
@@ -1,78 +1,78 @@
 import { HTMLtoSSML } from '../index';
 
 describe('striptags', () => {
-    test('headers', () => {
-        const converter = new HTMLtoSSML();
-        const html = "<h1>Test</h1>";
-        expect(converter.deleteHTML(html)).toBe(html);
-    });
-    test('change-allowedTags', () => {
-        const converter = new HTMLtoSSML();
-        converter.config.allowedTags = ['a']
-        let html = "<a>Test</a>";
-        expect(converter.deleteHTML(html)).toBe(html);
-        html = "<h1>Test</h1>";
-        expect(converter.deleteHTML(html)).toBe("Test");
-    });
+  test('headers', () => {
+    const converter = new HTMLtoSSML();
+    const html = '<h1>Test</h1>';
+    expect(converter.deleteHTML(html)).toBe(html);
+  });
+  test('change-allowedTags', () => {
+    const converter = new HTMLtoSSML();
+    converter.config.allowedTags = ['a'];
+    let html = '<a>Test</a>';
+    expect(converter.deleteHTML(html)).toBe(html);
+    html = '<h1>Test</h1>';
+    expect(converter.deleteHTML(html)).toBe('Test');
+  });
 });
 
 describe('realTest', () => {
-    test('simple', () => {
-        const converter = new HTMLtoSSML();
-        const html = "<html><body><div class='container'><p>Test article</p></div></body></html>";
-        const ssml = "<p>Test article</p>";
-        expect(converter.deleteHTML(html)).toBe(ssml);    
-    });
+  test('simple', () => {
+    const converter = new HTMLtoSSML();
+    const html = "<html><body><div class='container'><p>Test article</p></div></body></html>";
+    const ssml = '<p>Test article</p>';
+    expect(converter.deleteHTML(html)).toBe(ssml);
+  });
 });
 
-describe("replace tags", () => {
-    test("h1", () => {
-        const converter = new HTMLtoSSML();
-        const html = "<h1>Test</h1>";
-        expect(converter.convertHTMLTagsToSSMLTags(html)).toBe("<emphasis>Test</emphasis>");
-    });
-    test("h2", () => {
-        const converter = new HTMLtoSSML();
-        const html = "<h2>Test</h2>";
-        expect(converter.convertHTMLTagsToSSMLTags(html)).toBe("<emphasis>Test</emphasis>");
-    });
-    test("h3", () => {
-        const converter = new HTMLtoSSML();
-        const html = "<h3>Test</h3>";
-        expect(converter.convertHTMLTagsToSSMLTags(html)).toBe("<emphasis>Test</emphasis>");
-    });
-    test("br", () => {
-        const converter = new HTMLtoSSML();
-        const html = "Test<br>New line";
-        expect(converter.convertHTMLTagsToSSMLTags(html)).toBe("Test<break>New line");
-    })
+describe('replace tags', () => {
+  test('h1', () => {
+    const converter = new HTMLtoSSML();
+    const html = '<h1>Test</h1>';
+    expect(converter.convertHTMLTagsToSSMLTags(html)).toBe('<emphasis>Test</emphasis>');
+  });
+  test('h2', () => {
+    const converter = new HTMLtoSSML();
+    const html = '<h2>Test</h2>';
+    expect(converter.convertHTMLTagsToSSMLTags(html)).toBe('<emphasis>Test</emphasis>');
+  });
+  test('h3', () => {
+    const converter = new HTMLtoSSML();
+    const html = '<h3>Test</h3>';
+    expect(converter.convertHTMLTagsToSSMLTags(html)).toBe('<emphasis>Test</emphasis>');
+  });
+  test('br', () => {
+    const converter = new HTMLtoSSML();
+    const html = 'Test<br>New line';
+    expect(converter.convertHTMLTagsToSSMLTags(html)).toBe('Test<break>New line');
+  });
 });
 
-describe("wrap in speak tags", () => {
-    test("simple", () => {
-        const converter = new HTMLtoSSML();
-        const html = "Test";
-        expect(converter.wrapInSpeakTag(html)).toBe("<speak>Test</speak>");
-    })
+describe('wrap in speak tags', () => {
+  test('simple', () => {
+    const converter = new HTMLtoSSML();
+    const html = 'Test';
+    expect(converter.wrapInSpeakTag(html)).toBe('<speak>Test</speak>');
+  });
 });
 
-describe("complete test", () => {
-    test("simple 1", () => {
-        const converter = new HTMLtoSSML();
-        const html = "<html><body><div class='container'><p>Test article</p></div></body></html>";
-        const ssml = "<speak><p>Test article</p></speak>";
-        expect(converter.convert(html)).toBe(ssml);
-    });
-    test("simple 2", () => {
-        const converter = new HTMLtoSSML();
-        const html = "<html><body><div class='container'><h1>Title</h1><p>Test article</p></div></body></html>";
-        const ssml = "<speak><emphasis>Title</emphasis><p>Test article</p></speak>";
-        expect(converter.convert(html)).toBe(ssml);
-    });
-    test("simple 3", () => {
-        const converter = new HTMLtoSSML();
-        const html = "<html><body><div class='container'><h1>Title</h1><p>Test article<br>New line</p></div></body></html>";
-        const ssml = "<speak><emphasis>Title</emphasis><p>Test article<break>New line</p></speak>";
-        expect(converter.convert(html)).toBe(ssml);
-    });
+describe('complete test', () => {
+  test('simple 1', () => {
+    const converter = new HTMLtoSSML();
+    const html = "<html><body><div class='container'><p>Test article</p></div></body></html>";
+    const ssml = '<speak><p>Test article</p></speak>';
+    expect(converter.convert(html)).toBe(ssml);
+  });
+  test('simple 2', () => {
+    const converter = new HTMLtoSSML();
+    const html = "<html><body><div class='container'><h1>Title</h1><p>Test article</p></div></body></html>";
+    const ssml = '<speak><emphasis>Title</emphasis><p>Test article</p></speak>';
+    expect(converter.convert(html)).toBe(ssml);
+  });
+  test('simple 3', () => {
+    const converter = new HTMLtoSSML();
+    const html = "<html><body><div class='container'><h1>Title</h1><p>Test article<br>New line</p></div></body></html>";
+    const ssml = '<speak><emphasis>Title</emphasis><p>Test article<break>New line</p></speak>';
+    expect(converter.convert(html)).toBe(ssml);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,36 @@
 import * as striptags from 'striptags';
 
 export class HTMLtoSSML {
-    public config: { [id: string]: string[] } = {
-        allowedTags: ['h1', 'h2', 'h3', 'br', 'p']
-    }
+  public config: { [id: string]: string[] } = {
+    allowedTags: ['h1', 'h2', 'h3', 'br', 'p'],
+  };
 
-    public convert (html: string): string {
-        const stripped = this.deleteHTML(html);
-        const converted = this.convertHTMLTagsToSSMLTags(stripped);
-        return this.wrapInSpeakTag(converted);
-    }
+  public convert(html: string): string {
+    const stripped = this.deleteHTML(html);
+    const converted = this.convertHTMLTagsToSSMLTags(stripped);
+    return this.wrapInSpeakTag(converted);
+  }
 
-    public deleteHTML (html: string): string {
-        return striptags(html, this.config.allowedTags);
-    }
+  public deleteHTML(html: string): string {
+    return striptags(html, this.config.allowedTags);
+  }
 
-    public convertHTMLTagsToSSMLTags (html: string): string {
-        // Replace headings with emphasis
-        html = html.replace(/<h1>/g, '<emphasis>');
-        html = html.replace(/<h2>/g, '<emphasis>');
-        html = html.replace(/<h3>/g, '<emphasis>');
-        html = html.replace(/<\/h1>/g, '</emphasis>');
-        html = html.replace(/<\/h2>/g, '</emphasis>');
-        html = html.replace(/<\/h3>/g, '</emphasis>');
-        
-        // Replace lines with breaks
-        html = html.replace(/<br>/g, '<break>');
-        
-        return html;
-    }
+  public convertHTMLTagsToSSMLTags(html: string): string {
+    // Replace headings with emphasis
+    html = html.replace(/<h1>/g, '<emphasis>');
+    html = html.replace(/<h2>/g, '<emphasis>');
+    html = html.replace(/<h3>/g, '<emphasis>');
+    html = html.replace(/<\/h1>/g, '</emphasis>');
+    html = html.replace(/<\/h2>/g, '</emphasis>');
+    html = html.replace(/<\/h3>/g, '</emphasis>');
 
-    public wrapInSpeakTag (html: string): string {
-        return '<speak>' + html + '</speak>';
-    }
+    // Replace lines with breaks
+    html = html.replace(/<br>/g, '<break>');
+
+    return html;
+  }
+
+  public wrapInSpeakTag(html: string): string {
+    return '<speak>' + html + '</speak>';
+  }
 }
