71bf6fcfade32248dc9ec78b1a057ed4b6b5212a
Axel Peigné <axel@lamarckise.fr>
2021-01-17 15:00:27 +0100

    Renaming package

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 export declare class HTMLtoSSML {
     config: {
-        [id: string]: Array<string>;
+        [id: string]: string[];
     };
     convert(html: string): string;
     deleteHTML(html: string): string;
